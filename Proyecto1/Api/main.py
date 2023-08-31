926f20289c06075e7ff001bd7ee9f8c18c62da5f
findlescopy <findlesscopy@gmail.com>
2023-08-31 17:54:21 -0600

    "feat: peticiones agregadas; fix: base de datos"

--- Proyecto1/Api/main.py
+++ Proyecto1/Api/main.py
@@ -4,6 +4,12 @@ from datetime import datetime
 
 app = Flask(__name__)
 
+temperatura_actual = 0
+luz_actual = 0
+co2_actual = 0
+proximidad_actual = 0
+estado_luz = 0
+
 # Ruta para almacenar datos
 @app.route('/guardar_datos', methods=['POST'])
 def guardar_datos():
@@ -11,31 +17,29 @@ def guardar_datos():
         data = request.json
 
         temperatura = data['temperatura']
-        luz = data['co2']
-        humedad = data['luz']
-        calidad_aire = data['proximidad']
+        calidad_aire = data['co2']
+        luz = data['luz']
+        #proximidad = data['proximidad']
 
         # Conexión a la base de datos SQLite
-        conn = sqlite3.connect('datos.db')
+        conn = sqlite3.connect('data.db')
         cursor = conn.cursor()
 
         # Crear tabla si no existe
         cursor.execute('''
-            CREATE TABLE IF NOT EXISTS mediciones (
+            CREATE TABLE IF NOT EXISTS prueba (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 temperatura DECIMAL(5, 2) NOT NULL,
                 luz INTEGER NOT NULL,
-                humedad DECIMAL(5, 2) NOT NULL,
-                calidad_aire INTEGER NOT NULL,
-                fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+                calidad_aire INTEGER NOT NULL
             )
         ''')
 
         # Insertar datos en la tabla
         cursor.execute('''
-            INSERT INTO mediciones (temperatura, luz, humedad, calidad_aire)
-            VALUES (?, ?, ?, ?)
-        ''', (temperatura, luz, humedad, calidad_aire))
+            INSERT INTO prueba (temperatura, luz, calidad_aire)
+            VALUES (?, ?, ?)
+        ''', (temperatura, luz, calidad_aire))
 
         conn.commit()
         conn.close()
@@ -45,7 +49,52 @@ def guardar_datos():
     except Exception as e:
         return jsonify({"error": str(e)}), 400
 
+@app.route('/obtener_datos', methods=['GET'])
+def obtener_datos():
+    
+    cadena = ""
+    cadena += str(temperatura_actual) + "," + str(luz_actual) + "," + str(co2_actual)
 
+    return cadena
+
+@app.route('/enviar_datos', methods=['POST'])
+def enviar_datos():
+    try:
+        data = request.json
+
+        global temperatura_actual
+        nueva_temperatura_actual = data['temperatura']
+        temperatura_actual = nueva_temperatura_actual
+
+        global co2_actual
+        nuevo_co2_actual = data['co2']
+        co2_actual = nuevo_co2_actual
+
+        global luz_actual
+        nueva_luz_actual = data['luz']
+        luz_actual = nueva_luz_actual
+
+        return jsonify({"mensaje": "Datos actualizados correctamente"}), 201
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
+
+@app.route('/estado_luz', methods=['POST'])
+def estado_luz():
+    try:
+        data = request.json
+
+        response = data['estado']
+
+        if response == 0:
+            estado_luz = 0
+            return "Luces apagadas"
+        if response == 1:
+            estado_luz = 1
+            return "Luces encendidas"
+    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400 
 
 @app.route('/notificacion', methods=['POST'])
 def notificacion():
@@ -72,7 +121,7 @@ def notificacion():
 
 @app.route('/hola', methods=['GET'])
 def hola():
-    return jsonify({"mensaje": "Hola mundo"}), 200
+    return jsonify({"luz_actual" : luz_actual,"co2_actual": co2_actual,"temperatura_actual": temperatura_actual }), 200
 
 
 if __name__ == '__main__':
